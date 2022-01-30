fa0377916103ed140eb355e179e0dcc4ae6a4ddf
Pablo Barbero <pablo.barbero@glovoapp.com>
2022-01-30 21:25:38 +0100

    feat: add gcd solution

--- solutions/greatest_common_divisor/brute_force_solution.py
+++ solutions/greatest_common_divisor/brute_force_solution.py
@@ -1,6 +1,10 @@
-def brute_force_solution(n: int, k: int) -> int:
+from typing import Tuple
+
+
+def brute_force_solution(numbers: Tuple[int, int]) -> int:
+    n, k = numbers
     greatest_common_divisor = 1
-    for i in range(min([n, k])):
+    for i in range(1, min([n, k]) + 1):
         if n % i == 0 and k % i == 0:
             greatest_common_divisor = i
     return greatest_common_divisor
