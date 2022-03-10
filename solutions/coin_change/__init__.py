7f4661d0a9f6290b958092814f2fd895c0ae6a5c
Pablo Barbero <pablo.barbero@glovoapp.com>
2022-03-10 09:16:56 +0100

    feat: add solution to coin change challenge

---
+++ solutions/coin_change/__init__.py
@@ -0,0 +1,7 @@
+from solutions.coin_change.brute_force_solution import brute_force_solution
+from solutions.coin_change.efficient_solution import efficient_solution
+
+__all__ = [
+    "brute_force_solution",
+    "efficient_solution",
+]
---
+++ solutions/coin_change/constants.py
@@ -0,0 +1,3 @@
+from typing import List
+
+COIN_CHANGE: List[int] = [1, 5, 10]
