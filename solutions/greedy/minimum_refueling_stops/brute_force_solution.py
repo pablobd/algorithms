1cb66d7494051527122de8dc284544380dff892b
pablobd <pablo.barbero@glovoapp.com>
2022-04-22 10:10:23 +0200

    feat: implement brute force solution

--- solutions/greedy/minimum_refueling_stops/brute_force_solution.py
+++ solutions/greedy/minimum_refueling_stops/brute_force_solution.py
@@ -2,7 +2,30 @@ from typing import List, Tuple
 
 
 def brute_force_solution(
-    input_min_refueling_stops: Tuple[float, float, List[int]]
+    input_min_refueling_stops: Tuple[float, float, List[float]]
 ) -> int:
+    """Solve the problem using brute force.
+
+    Parameters
+    ----------
+    input_min_refueling_stops : Tuple[float, float, List[int]]
+        The input data, a tuple of the form (distance, tank_capacity, stations).
+
+    Returns
+    -------
+    int
+        The minimum number of refueling stops.
+    """
     destination_distance, tank_capacity, gas_stations = input_min_refueling_stops
-    return 1
+    stops = [0.0] + gas_stations + [destination_distance]
+    refuel = 0
+    i = 0
+    while i + 2 < len(stops):
+        for j, next_gas_station in enumerate(stops[(i + 1) :]):
+            if tank_capacity < next_gas_station - stops[i]:
+                if j == 0:
+                    return -1
+                refuel += 1
+                break
+        i += j
+    return refuel
--- tests/greedy/minimum_refueling_stops/test_brute_force_solution.py
+++ tests/greedy/minimum_refueling_stops/test_brute_force_solution.py
@@ -5,5 +5,6 @@ def test_brute_force_solution():
     assert 2 == minimum_refueling_stops.brute_force_solution(
         (950, 400, [200, 375, 550, 750])
     )
+
     assert -1 == minimum_refueling_stops.brute_force_solution((10, 3, [1, 2, 5, 9]))
     assert 0 == minimum_refueling_stops.brute_force_solution((200, 250, [100, 150]))
