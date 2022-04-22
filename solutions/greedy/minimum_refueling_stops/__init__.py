7fda3e04d299317d9f7815b19e607c9c9cebbd7e
pablobd <pablo.barbero@glovoapp.com>
2022-04-22 08:49:39 +0200

    feat: add tests

---
+++ solutions/greedy/minimum_refueling_stops/__init__.py
@@ -0,0 +1,5 @@
+from solutions.greedy.minimum_refueling_stops.brute_force_solution import (
+    brute_force_solution,
+)
+
+__all__ = ["brute_force_solution"]
--- solutions/greedy/minimum_refueling_stops/brute_force_solution.py
+++ solutions/greedy/minimum_refueling_stops/brute_force_solution.py
@@ -1,5 +1,8 @@
-from typing import Tuple
+from typing import List, Tuple
 
 
-def brute_force_solution(input_min_refueling_stops: Tuple) -> int:
-    pass
+def brute_force_solution(
+    input_min_refueling_stops: Tuple[float, float, List[int]]
+) -> int:
+    destination_distance, tank_capacity, gas_stations = input_min_refueling_stops
+    return 1
